08147d42e2298f08e85b9a36bf45c118250cbe0a
AhmedES247 <154126408+ahmedES247@users.noreply.github.com>
2024-11-16 10:53:24 +0530

    Create switch.js

---
+++ switch.js
@@ -0,0 +1,24 @@
+let word = 1
+switch (word) {
+    case 1:
+        console.log("Right"); // Switch will check the case. 
+        break;
+    case 2:
+        console.log("Wrong");
+} // OutPut is Right
+
+
+let day = "Sunday"
+switch (day) {
+    case "Monday":
+        console.log("Monday"); // Switch will check the case. 
+        break;
+    case "Tuesday":
+        console.log("Tuesday");
+        break;
+    case "Saturday":
+        console.log("Saturday");
+        break;
+    case "Sunday":
+        console.log("Sunday"); // Switch will check the case and when it match then it will print in console.
+} //OutPut is Sunday
