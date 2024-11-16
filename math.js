3ecd9d7259e6e501983cb25f0a0aa070dd599b83
AhmedES247 <154126408+ahmedES247@users.noreply.github.com>
2024-11-16 10:42:39 +0530

    Create math.js

---
+++ math.js
@@ -0,0 +1,14 @@
+let add = 2000;
+console.log(add + 20); // + add
+
+
+let subtract = 2020; 
+console.log(subtract - 20); // - subtract
+
+
+let multiply = 7;
+console.log(multiply * 3); // * multiply
+
+
+let devide = 21;
+console.log(devide / 7); // / devide
