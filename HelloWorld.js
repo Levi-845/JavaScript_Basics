65de87bd3dc1f8fe2c6080bd91e859b7062af931
AhmedES247 <154126408+ahmedES247@users.noreply.github.com>
2024-11-16 10:26:18 +0530

    Create HelloWorld.js

---
+++ HelloWorld.js
@@ -0,0 +1,6 @@
+console.log("Hello World"); // Hello World program
+// OutPut :- Hello World
+
+let myName = "Hello World" // i made Hello World variable and then print it.
+console.log(myName);
+// OutPut :- Hello World
