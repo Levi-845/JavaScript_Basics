6609b44aa4d047a8eddf2bb8ac3dfb2052f19c43
AhmedES247 <154126408+ahmedES247@users.noreply.github.com>
2024-11-16 10:40:28 +0530

    Create if_else-if_else.js

---
+++ if_else-if_else.js
@@ -0,0 +1,15 @@
+let one = 1
+if(one == 1) {
+    console.log("correct")
+} else {
+    console.log("incorrect")
+} // OutPut :- correct
+
+let two = 7
+if(two > 2){
+    console.log("Right")
+} else if (two < 6) {
+    console.log("Wrong")
+} else {
+    console.log("None")
+} // OutPut :- Right
