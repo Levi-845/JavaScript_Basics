6922e7b3738bd11f3fa9a56566582f4be398048a
AhmedES247 <154126408+ahmedES247@users.noreply.github.com>
2024-11-16 10:41:49 +0530

    Create calculation.js

---
+++ calculation.js
@@ -0,0 +1,20 @@
+let add = 2000;
+console.log(add + 20); // (+) add
+
+let subtract = 2020; 
+console.log(subtract - 20); // (-) subtract
+
+let multiply = 7;
+console.log(multiply * 3); // (*) multiply
+
+let devide = 21;
+console.log(devide / 7); // (/) devide
+//OutPut in the Console :- 2020, 2000, 21, 3
+
+
+num1 = 98
+num2 = 34
+num3 = 46
+num4 = 76
+document.write(num1 * num2 + num3 - num4); // document.write gives OutPut direct on browser window, not in the Console.
+// OutPut :- 3302
