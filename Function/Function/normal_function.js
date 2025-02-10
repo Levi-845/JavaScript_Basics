7c7695a67495f43efb6bac932f71791879513722
Ahmed Naviwala <154126408+ahmedST247@users.noreply.github.com>
2025-02-10 19:28:26 +0530

    Create normal_function.js

---
+++ Function/Function/normal_function.js
@@ -0,0 +1,32 @@
+function myName() {
+    document.write("My Name is Ahmed");
+    document.write("<br>");
+    document.write("Web Developer");
+    document.write("<br>");
+    document.write("Full Stack Developer");
+    document.write("<br>");
+    document.write("Python & JavaScript Expert");
+    document.write("<br>");
+    document.write("<br>");
+};
+myName();
+// OutPut :- My name is ahmed
+// Web Developer 
+// Full Stack Developer
+// Python & JavaScript Expert
+
+
+function fullName(firstName, LastName) {
+    document.write("Hi! " + firstName + " " + LastName);
+    document.write("<br>");
+};
+fullName('Ahmed', 'Naviwala');
+// OutPut :- Hi! Ahmed Naviwala
+
+
+function helloWorld(word1, word2) {
+    document.write("<br>");
+    document.write(word1 + word2);
+};
+helloWorld("Hello ","World");
+// OutPut :- Hello World
