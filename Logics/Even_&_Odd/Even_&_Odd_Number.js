8fed0ac7dccc25b82263d7bdb6cf145dff2d1ac6
Levi <naviwalaahmed009@gmail.com>
2025-08-20 10:42:44 +0530

    Create Even_&_Odd_Number.js

---
+++ Logics/Even_&_Odd/Even_&_Odd_Number.js
@@ -0,0 +1,19 @@
+let n = 2;   //Even Number.
+
+if (n % 2 === 0){    // Even Number Logic
+    console.log(n + " is Even Number")
+} else {
+    console.log(n + " is Odd Number")
+}
+
+// ------------------------------------------
+
+let num = 7;   // Odd Number.
+
+if (num % 2 === 1){    // Odd Number Logic
+    console.log(num + " is Odd Number")
+} else {
+    console.log(num + " is Even Number")
+}
+
+
