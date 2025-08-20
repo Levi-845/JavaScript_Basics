dad018ee001b53766481467da5aea8531bb887b1
Levi <naviwalaahmed009@gmail.com>
2025-08-20 10:59:19 +0530

    Create index.js

---
+++ Logics/Voting/index.js
@@ -0,0 +1,24 @@
+// Storing votes for two Candidates A and B
+let candidateA = 0;
+let candidateB = 0;
+
+// 5 voters choice (A or B)
+let votes = ["A", "B", "A", "A", "B"];
+
+// Counting votes for each candidete.
+for (let vote of votes) {
+  if (vote === "A") {
+    candidateA++;
+  } else if (vote === "B") {
+    candidateB++;
+  }
+}
+
+// Checking Results.
+if (candidateA > candidateB) {
+  console.log("Candidate A Wins 🏆");
+} else if (candidateB > candidateA) {
+  console.log("Candidate B Wins 🏆");
+} else {
+  console.log("It's a Tie 🤝");
+}
